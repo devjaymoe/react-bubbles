2ee8b9c1112a9bbc11b3c469a73665360d3d7fe0
Devin_Moore <d.j.moore209@gmail.com>
2020-04-24 12:06:33 -0400

    added put request for color editor

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,6 +22,17 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        const newColor = res.data
+        const newColorList = colors.filter(color => color.id !== newColor.id)
+        updateColors([
+          ...newColorList,
+          newColor
+        ])
+      })
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
